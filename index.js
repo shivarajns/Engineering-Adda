a1db66afa3efc9133e82e8bd66f4d51bd29df51e
Shivaraju NS <159640509+shivarajns@users.noreply.github.com>
2025-04-10 18:24:43 +0530

    uploaded all the files with nav bar updation
    a nav bar will be updated to make user interface easy

---
+++ index.js
@@ -0,0 +1,30 @@
+let user_password;
+let correctness;
+let check=0;
+let password = 'ECE6TH';
+
+
+
+document.getElementById("submit").onclick = function () {
+    user_password=document.getElementById("UserPassword").value;
+    user_password= user_password.toUpperCase();
+    console.log(user_password);
+
+    if (user_password === password){
+        console.log(password)
+        window.location.href = "sem_6_studiMaterials.html";
+        console.log("redirect done")
+        }
+    else{
+        alert("Incorrect Password, Try again")
+        location.reload();
+    }
+
+};
+
+function clear() {
+    return ""
+}
+
+
+// let menu=document.getElementById("menubar");
